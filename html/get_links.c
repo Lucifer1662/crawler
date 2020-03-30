aec4260d38e5c95793d26ab9f09cb1b2c5f60f46
Luke Hawkins <l.hawk99@hotmail.com>
2020-03-30 14:53:28 +1100

    added more tests and handled capital href, captial anchor and single quotes in html

--- html/get_links.c
+++ html/get_links.c
@@ -5,9 +5,12 @@
 #include "../vector/vector_strings.h"
 
 #define ANCHOR_START "<a"
+#define ANCHOR_START_CAP "<A"
 #define ANCHOR_END ">"
 #define HREF "href"
+#define HREF_CAP "HREF"
 #define QUOTE '"'
+#define SINGLE_QUOTE '\''
 
 int is_in_quotes(char *str, char *location, char *end) {
     int res = 0;
@@ -30,13 +33,46 @@ int is_in_quotes(char *str, char *location, char *end) {
     return res;
 }
 
-// returns a vector of char*
+void convert_single_quote_to_quote(char *str) {
+    for (; *str; str++) {
+        if (*str == SINGLE_QUOTE) {
+            *str = QUOTE;
+        }
+    }
+}
+
+void convert_anchor_to_lower(char *str) {
+    char *anchor_start_cap;
+    int len_anchor_start_cap = strlen(ANCHOR_START_CAP);
+    // while there is capital anchors
+    while ((anchor_start_cap = strstr(str, ANCHOR_START_CAP)) != NULL) {
+        // replace cap with lower
+        for (int i = 0; i < len_anchor_start_cap; i++) {
+            anchor_start_cap[i] = ANCHOR_START[i];
+        }
+    }
+}
+
 vector get_links(char *html) {
+    html = strdup(html);
+    convert_single_quote_to_quote(html);
+    convert_anchor_to_lower(html);
+
     vector links = create_vector_strings();
+    get_links_symbols(&links, html, HREF);
+    get_links_symbols(&links, html, HREF_CAP);
+
+    free(html);
+    return links;
+}
+
+void get_links_symbols(vector *links, char *html, char *href_symbol) {
     while (*html) {
         // Find anchor start
         char *anchor_start = strstr(html, ANCHOR_START);
-        if (anchor_start == NULL) break;
+        if (anchor_start == NULL) {
+            break;
+        }
 
         // Find anchor end
         char *anchor_end = strstr(anchor_start, ANCHOR_END);
@@ -48,16 +84,16 @@ vector get_links(char *html) {
 
         int valid_href = 0;
         while (1) {
-            href = strstr(href, HREF);
+            href = strstr(href, href_symbol);
+
             if (href == NULL) break;
             if (anchor_start < href && href < anchor_end &&
                 !is_in_quotes(anchor_start, href, anchor_end)) {
-                    valid_href = 1;
-                    break;
+                valid_href = 1;
+                break;
             }
-            href += strlen(HREF);
+            href += strlen(href_symbol);
         }
-        
 
         if (valid_href) {
             // if inside
@@ -72,10 +108,9 @@ vector get_links(char *html) {
             // extract link and push
             int linkLength = closingQuote - openingQuote;
             char *link = strndup(openingQuote, linkLength);
-            push_back(&links, &link);
+            push_back_str(links, link);
         }
         // move html to end of anchor
         html = anchor_end + 1;
     }
-    return links;
 }
---
+++ html/run_all.bat
@@ -0,0 +1,6 @@
+make & test test1.html test1.txt & 
+test test2.html test2.txt &
+test test3.html test3.txt & 
+test test4.html test4.txt & 
+test test5.html test5.txt &
+test test6.html test6.txt
